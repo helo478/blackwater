e0de1fac2b6b2edd27acb6850f03435a234f0fec
Donald Subert <helo478@gmail.com>
2016-07-04 21:48:06 -0700

    used concatenation for AngularJS and application CSS code

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,14 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: ['public'],
-    copy: { // Task that copies the client application code to the public folder
+    clean: ['public/*'],
+    copy: { // Task that copies the html partials
       app: {
         files: [
           {
             expand: true,
             cwd: 'client',
-            src: '**/*',
+            src: '**/*.html',
             dest: 'public'
           }
         ]
@@ -21,17 +21,33 @@ module.exports = function(grunt) {
         separator: ';'
       },
       jslib: {
-        src: [
+        src: [ // List all of the javascript libraries in node_modules that the client needs
           'node_modules/angular/angular.min.js',
           'node_modules/angular-route/angular-route.min.js',
           'node_modules/jquery/dist/jquery.min.js',
           'node_modules/bootstrap/dist/js/bootstrap.min.js'
         ],
-        dest: 'public/lib.js'
+        dest: 'public/lib.js' // They will all be concatenated in a single file here
       },
       csslib: {
-        src: ['node_modules/bootstrap/dist/css/bootstrap.min.css'],
+        src: ['node_modules/bootstrap/dist/css/bootstrap.min.css'], // Same for CSS libraries
         dest: 'public/lib.css'
+      },
+      ngApp: {
+        src: [
+          'client/modules/**/*.config.js',
+          'client/modules/**/*.service.js',
+          'client/modules/**/*.factory.js',
+          'client/modules/**/*.filter.js',
+          'client/modules/**/*.ctrl.js',
+          'client/modules/**/*.directive.js',
+          'client/ng-app.js'
+        ],
+        dest: 'public/ng-app.js'
+      },
+      css: {
+        src: ['client/**/*.css'],
+        dest: 'public/application.css'
       }
     },
     watch: { // Task that updates client application code in the public folder in real time
@@ -58,5 +74,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s). Copy code to the public folder and update as necessary.
-  grunt.registerTask('default', ['clean', 'copy', 'concat', 'watch']);
+  grunt.registerTask('default', ['copy', 'concat', 'watch']);
 };
